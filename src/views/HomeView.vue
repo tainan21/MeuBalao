483e9afb29f37a90be68bf5083df302f8e5049da
Tai <tainancmoraes@gmail.com>
2022-07-24 18:04:34 -0300

    alter frist comimt menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,6 @@
   <div class="home">
     <h1> Meu Balão </h1>
     <h1> Passeio de Balão </h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
